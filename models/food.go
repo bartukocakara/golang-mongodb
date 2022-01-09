3ef9a129d6c84c5e7ffaab3708d40330503bef9e
bartukocakara <kocakarabartu@gmail.com>
2022-01-10 00:36:18 +0300

    models edited

--- models/food.go
+++ models/food.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Food struct {
+	ID   primitive.ObjectID	`bson:"_id"`
+	Name *string 	`json:"name" validate:"required,min=2,max10"`
+	Price *float64	`json:"price" validate:"required"`
+	FoodImage *string  `json:"food_image" validate:"required"`
+	CreatedAt time.Time `json:"created_created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	FoodID *string `json:"food_id"`
+	MenuId *string `json:"menu_id" validate:"required"`
+}
